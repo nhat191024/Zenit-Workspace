e8ba102f56b4026676e535736a0fbe76f4ff3cf5
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-04-22 18:59:41 +0700

    fix: update FilamentExceptionsPlugin instantiation to use make() method

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -54,7 +54,7 @@ class AdminPanelProvider extends PanelProvider
             ])
 
             ->plugins([
-                FilamentExceptionsPlugin::class,
+                FilamentExceptionsPlugin::make(),
             ])
 
             ->middleware([
